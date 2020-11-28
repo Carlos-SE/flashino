31323f60227474eaaa7fcd3fd25d93f216b4d48d
Carlos Salas <csalaseiroa@correo.ugr.es>
2020-11-28 18:29:58 +0100

    Testing

---
+++ SFlash.ino
@@ -0,0 +1,124 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//    To do: finish(), complete class Seq
+//
+//
+//
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// green -> 0
+// yellow -> 1
+// Blue -> 2
+// Red -> 3
+
+#define GREEN 0
+#define YELLOW 1
+#define BLUE 2
+#define RED 3
+
+// Leds (OUTPUTS)
+int GreenLed = 3;
+int YellowLed = 4;
+int BlueLed = 5;
+int RedLed = 6;
+
+// Buttons (INPUTS)
+int GreenB = 7;
+int YellowB = 8;
+int BlueB = 9;
+int RedB = 10;
+
+
+// Object Color
+class Color{
+private:
+  int value = -1;
+
+public:
+
+  Color(void){
+    value = random(0,4);
+  }
+  Color(int val){
+    value = val;
+  }
+
+  int getValue(void){
+    return value;
+  }
+
+
+};
+
+
+// Object sequence
+
+class Seq{
+private:
+  Color * sequence;
+  Color * userInput;
+  int sizee;
+  int utils;
+  bool correct = true;
+public:
+  Seq(void){
+    sequence = new Color[0];
+    userInput = new Color[0];
+    sizee = 0;
+    utils = -1;
+  }
+  Seq(int elements){
+    sequence = new Color[elements];
+    userInput = new Color[elements];
+    sizee = elements;
+    utils = 0;
+  }
+  bool addColor(Color color){
+    bool result = false;
+    
+    if (utils+1 < sizee){
+      sequence[utils] = color;
+      
+      result = true;
+    }
+    return result;
+  }
+
+  bool addUserInput(Color color){
+    bool result = false;
+
+    if (utils+1 = sizee){
+      userInput[utils] = color;
+      checkCorrect();
+      result = true;
+    }
+
+    return result;
+  }
+
+  // If any input was incorrect finish the game;
+  void checkCorrect(){
+    for (int i = 0; i < utils; i++){
+      if (sequence[i] != userInput[i]){
+        finish();
+      }
+    }
+  }
+
+  
+};
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
