e1dd8dd70a8fa2c0899ee368a88e6195093c055f
Carlos Salas <csalaseiroa@correo.ugr.es>
2021-04-22 10:11:58 +0200

    initial sketch

--- SFlash.ino
+++ SFlash.ino
@@ -1,98 +1,27 @@
-///////////////////////////////////////////////////////////////////////////////
-//
-//    To do: void finish(), void checkCorrect(),  complete class Seq, 
-//
-//
-//
-//
-///////////////////////////////////////////////////////////////////////////////
-
-// green -> 0
-// yellow -> 1
-// Blue -> 2
-// Red -> 3
-
-#define GREEN 0
-#define YELLOW 1
-#define BLUE 2
-#define RED 3
-
-// Leds (OUTPUTS)
-int GreenLed = 3;
-int YellowLed = 4;
-int BlueLed = 5;
-int RedLed = 6;
-
-// Buttons (INPUTS)
-int GreenB = 7;
-int YellowB = 8;
-int BlueB = 9;
-int RedB = 10;
-
-
-// Object Color
-class Color{
-private:
-  int value = -1;
-
-public:
-
-  Color(void){
-    value = random(0,4);
-  }
-  Color(int val){
-    value = val;
-  }
-
-  int getValue(void){
-    return value;
-  }
-
-
-};
-
-
-// Object sequence
-
-class Seq{
-private:
-  Color * sequence;
-  Color * userInput;
-  int sizee;
-  int utils;
-  bool correct = true;
-public:
-  Seq(void){
-    sequence = new Color[0];
-    sizee = 0;
-    utils = -1;
-  }
-  Seq(int elements){
-    sequence = new Color[elements];
-    sizee = elements;
-    utils = 0;
-  }
-  bool addColor(Color color){
-    bool result = false;
-    
-    if (utils+1 < sizee){
-      sequence[utils] = color;
-      
-      result = true;
-    }
-    return result;
-  }
-
-
-  
-};
-
-
-
+#include "classes.ino"
 
 
 void setup() {
-  // put your setup code here, to run once:
+
+  int i;
+
+  Seq simon(DEFAULT_LEVELS);
+  
+  //Generates the color sequence
+  for (i = 0; i < DEFAULT_LEVELS; i++){
+    Color c;
+    simon.addColor(c);
+  }
+
+  pinMode(GreenLed, OUTPUT);
+  pinMode(YellowLed, OUTPUT);
+  pinMode(BlueLed, OUTPUT);
+  pinMode(RedLed, OUTPUT);
+
+  pinMode(GreenB, INPUT);
+  pinMode(YellowB, INPUT);
+  pinMode(BlueB, INPUT);
+  pinMode(RedB, INPUT);
 
 }
 
---
+++ classes.ino
@@ -0,0 +1,132 @@
+// green -> 0
+// yellow -> 1
+// Blue -> 2
+// Red -> 3
+#define GREEN 0
+#define YELLOW 1
+#define BLUE 2
+#define RED 3
+#define DEFAULT_LEVELS 15
+// Leds (OUTPUTS)
+const int GreenLed = 3;
+const int YellowLed = 4;
+const int BlueLed = 5;
+const int RedLed = 6;
+
+// Buttons (INPUTS)
+const int GreenB = 7;
+const int YellowB = 8;
+const int BlueB = 9;
+const int RedB = 10;
+
+
+// Object Color
+class Color{
+private:
+  int value = -1;
+
+public:
+
+  Color(void){
+    value = random(0,4);
+  }
+  Color(int val){
+    value = val;
+  }
+
+  int getValue(void){
+    return value;
+  }
+
+
+};
+
+
+// Object sequence
+
+class Seq{
+
+private:
+  Color * sequence;
+  Color * userInput;
+  int sizee;
+  int utilsSeq;
+  int utilsUser;
+  bool correct = true;
+
+public:
+  Seq(void){
+    sequence = new Color[0];
+    sizee = 0;
+    utilsSeq = -1;
+    utilsUser = -1;
+  }
+
+  Seq(int elements){
+    sequence = new Color[elements];
+    sizee = elements;
+    utilsSeq = 0;
+    utilsUser = 0;
+  }
+
+  bool addColor(Color color){
+    bool result = false;
+    
+    if (utilsSeq+1 < sizee){
+      sequence[utilsSeq] = color;
+      utilsSeq++;
+      
+      result = true;
+    }
+
+    return result;
+  }
+
+  bool addUserInput(Color color){
+    bool result = false;
+
+    if (utilsUser+1 < sizee){
+      userInput[utilsUser] = color;
+      utilsUser++;
+
+      result = true;
+    }
+
+    return result;
+  }
+
+  Color getSeqColor(int i){
+    return sequence[i];
+  }
+
+  void displaySeq(int l){
+    int i, j;
+    int colorLed;
+    Color c;
+
+    for (i = 0; i < l; i++){
+      for (j = 0; j <= i; j++){
+        c = getSeqColor(j);
+
+        if (c.getValue() == GREEN){
+          colorLed = GreenLed;
+        }
+        else if (c.getValue() == YELLOW){
+          colorLed = YellowLed;
+        }
+        else if (c.getValue() == BLUE){
+          colorLed = BlueLed;
+        }
+        else{
+          colorLed = RedLed;
+        }
+
+        digitalWrite(colorLed, HIGH);
+        delay(500);
+        digitalWrite(colorLed, LOW);
+        delay(500);
+      }
+    }
+  }
+  
+};
